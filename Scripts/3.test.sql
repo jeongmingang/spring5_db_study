5e75d495e5daae7ed740745ae49187b84b060de1
mingang <j01036733521@gmail.com>
2021-05-18 10:22:05 +0900

    트랜잭션 처리

--- Scripts/3.test.sql
+++ Scripts/3.test.sql
@@ -2,3 +2,5 @@ desc member;
 
 select * from member;
 
+delete from member where ID = 24;
+
--- src/main/java/spring5_db_study/spring/ChangePasswordService.java
+++ src/main/java/spring5_db_study/spring/ChangePasswordService.java
@@ -1,27 +1,21 @@
 package spring5_db_study.spring;
 
+import org.springframework.transaction.annotation.Transactional;
+
 public class ChangePasswordService {
 	private MemberDao memberDao;
 	
-	public ChangePasswordService() {
-		
-	}
-	
-	public ChangePasswordService(MemberDao memberDao) {
+	public void setMemberDao(MemberDao memberDao) {
 		this.memberDao = memberDao;
 	}
 
+	@Transactional
 	public void changePassword(String email, String oldPwd, String newPwd) {
 		Member member = memberDao.selectByEmail(email);
 		if(member == null) {
 			throw new MemberNotFoundException();
 		}	
 		member.changePassword(oldPwd, newPwd);
-		
 		memberDao.update(member);
-	}
-
-	public void setMemberDao(MemberDao memberDao) {
-		this.memberDao = memberDao;
 	}
 }
